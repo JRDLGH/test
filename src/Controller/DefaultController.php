730b727ca9be7ad68b90e074106eec7963b568d0
JRDLGH <ipes.lagache@gmail.com>
2017-12-24 17:57:20 +0100

    Adding annotations (replace routes in routes.yaml)

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,7 +2,9 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-
+/**
+ * @Route(/hello/{name})
+ */
 class DefaultController{
     public function index($name){
             return new Response('Hello '.$name.'!');
