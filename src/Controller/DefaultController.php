f1ea72bf4b0db003c7c473803886646e510776f7
JRDLGH <ipes.lagache@gmail.com>
2017-12-24 19:12:33 +0100

    Added logger

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,12 +3,17 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Psr\Log\LoggerInterface;
 
-class DefaultController{
+class DefaultController extends AbstractController{
     /**
      * @Route("/hello/{name}")
      */
-    public function index($name){
-            return new Response('Hello '.$name.'!');
+    public function index($name, LoggerInterface $logger){
+        $logger->info("Say hello to $name");
+            return $this->render('default/index.html.twig',[
+                'name'=>$name,
+            ]);
     }
 }
