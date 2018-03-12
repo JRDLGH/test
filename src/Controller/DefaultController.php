2507b66e65e5b4651c4909b4c94cda8897cd1845
Jordan Lagache <j.lagache@bigbadwolf.be>
2018-03-12 12:00:38 +0100

    adding default route

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -10,10 +10,17 @@ class DefaultController extends AbstractController{
     /**
      * @Route("/hello/{name}")
      */
-    public function index($name, LoggerInterface $logger){
+    public function hello($name, LoggerInterface $logger){
         $logger->info("Say hello to $name");
             return $this->render('default/index.html.twig',[
                 'name'=>$name,
             ]);
+    }
+    /**
+     * @Route("/",name="home")
+     * @Route("/home",name="app_home")
+     */
+    public function home(){
+        return $this->render('base.html.twig');
     }
 }
